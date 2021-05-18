0644257826d1c697675e3040ada1a1bbe0a52774
eeeibra <eeeibrahimsahin@gmail.com>
2021-05-18 16:03:58 +0200

    Lesson18-5-2021

---
+++ src/main/java/com/company/Room3/Model.java
@@ -0,0 +1,15 @@
+package com.company.Room3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Model {
+    public List<Boat> boatList = new ArrayList<>();
+    public List<Employee> employeeList = new ArrayList<>();
+    public List<Client> clientList = new ArrayList<>();
+    public List<Order> orderList = new ArrayList<>();
+    public List<BoatType> boatTypeList = new ArrayList<>();
+
+
+}

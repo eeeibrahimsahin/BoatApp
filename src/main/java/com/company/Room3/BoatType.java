d39067bbb532d9543eb05e4c5b583b0209e7a654
eeeibra <eeeibrahimsahin@gmail.com>
2021-05-19 21:38:09 +0200

    19-05-2021

--- src/main/java/com/company/Room3/BoatType.java
+++ src/main/java/com/company/Room3/BoatType.java
@@ -1,56 +1,8 @@
 package com.company.Room3;
 
-public class BoatType {
-    private String type;
-    private int seats;
-    private int chargingTime;
-    private double minimumPricePerHour;
-
-    public BoatType() {
-    }
-
-    public BoatType(String type, int seats, double minimumPricePerHour) {
-        this.type = type;
-        this.seats = seats;
-        this.minimumPricePerHour = minimumPricePerHour;
-    }
-
-    public BoatType(String type, int seats, int chargingTime, double minimumPricePerHour) {
-        this.type = type;
-        this.seats = seats;
-        this.chargingTime = chargingTime;
-        this.minimumPricePerHour = minimumPricePerHour;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getSeats() {
-        return seats;
-    }
-
-    public void setSeats(int seats) {
-        this.seats = seats;
-    }
-
-    public int getChargingTime() {
-        return chargingTime;
-    }
-
-    public void setChargingTime(int chargingTime) {
-        this.chargingTime = chargingTime;
-    }
-
-    public double getMinimumPricePerHour() {
-        return minimumPricePerHour;
-    }
-
-    public void setMinimumPricePerHour(double minimumPricePerHour) {
-        this.minimumPricePerHour = minimumPricePerHour;
-    }
+public enum BoatType {
+    KAJAK,
+    ROWINGBOAT,
+    SUPBOARD,
+    ELECTRICALBOAT
 }
